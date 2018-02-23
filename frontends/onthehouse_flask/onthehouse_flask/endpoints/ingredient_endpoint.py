49955605c965953c57058f9a22243d6c0fb44d90
whoshaddady <35232592+whoshaddady@users.noreply.github.com>
2018-02-22 20:27:51 -0800

    Update ingredient_endpoint.py

--- frontends/onthehouse_flask/onthehouse_flask/endpoints/ingredient_endpoint.py
+++ frontends/onthehouse_flask/onthehouse_flask/endpoints/ingredient_endpoint.py
@@ -6,7 +6,7 @@ from . import common
 
 site = common.site
 
-@site.route('/recipe/<ingredientname>')
+@site.route('/ingredient/<ingredientname>')
     def get_ingredient(ingredientname):
         ingredient = common.rdb.get_ingredient_by_name(ingredientname)
         response = render_template("ingredient.html", ingredient=ingredient, session_user=common.get_session(request))
