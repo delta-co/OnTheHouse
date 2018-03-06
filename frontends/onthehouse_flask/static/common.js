d33e61dc72cce0a0514234c41e521e1317972288
Ethan Dalool <edalool@yahoo.com>
2018-03-06 11:54:50 -0800

    Move apply_ingredient_autosuggest to common.js.

--- frontends/onthehouse_flask/static/common.js
+++ frontends/onthehouse_flask/static/common.js
@@ -1,3 +1,61 @@
+function apply_ingredient_autosuggest(element)
+{
+    console.log("Test");
+    var autosuggest_div = document.createElement("div");
+    autosuggest_div.classList.add("ingredient-autosuggest-list");
+    autosuggest_div.classList.add("hidden");
+    //autosuggest_div.innerText = "HO HO HO";
+    element.__autosuggest_div = autosuggest_div;
+    function hide_div(){
+        //console.log("hiding autosuggest");
+        autosuggest_div.classList.add("hidden");
+    };
+    function show_div(){
+        //console.log("showing autosuggest");
+        var cont_rect = element.parentElement.getBoundingClientRect();
+        var elem_rect = element.getBoundingClientRect();
+        //console.log(cont_rect);
+        //console.log(elem_rect);
+        autosuggest_div.style.top = elem_rect.bottom - cont_rect.top;
+        autosuggest_div.style.left = elem_rect.left - cont_rect.left;
+        autosuggest_div.classList.remove("hidden");
+    };
+    function update_suggestions()
+    {
+        var value = element.value;
+        value = value.split(/,/g);
+        value = value[value.length - 1];
+        value = value.trim();
+        value = value.toLocaleLowerCase();
+
+        var suggestions = [];
+        if (value !== "")
+        {
+            for (var index = 0; index < ALL_INGREDIENTS.length; index += 1)
+            {
+                var ingredient = ALL_INGREDIENTS[index];
+                if (ingredient.toLocaleLowerCase().indexOf(value) != -1)
+                {
+                    suggestions.push(ingredient);
+                }
+            }
+        }
+        if (suggestions.length > 0)
+        {
+            autosuggest_div.innerHTML = suggestions.join("<br>");
+            show_div();
+        }
+        else
+        {
+            hide_div();
+        }
+    }
+    element.addEventListener("focus", update_suggestions);
+    element.addEventListener("blur", hide_div);
+    element.addEventListener("keyup", update_suggestions);
+    element.parentElement.appendChild(autosuggest_div);
+}
+
 function bind_enter(box, button)
 {
     var hook = function(event)
