2bf8d0f4806d18b975da7bd9bbcddf67ac4d2635
Ethan Dalool <edalool@yahoo.com>
2018-03-06 11:27:20 -0800

    Add ingredient autosuggest to the search forms!

--- frontends/onthehouse_flask/static/common.js
+++ frontends/onthehouse_flask/static/common.js
@@ -10,6 +10,28 @@ function bind_enter(box, button)
     box.addEventListener("keyup", hook);
 }
 
+function get(url, callback)
+{
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+        if (request.readyState == 4)
+        {
+            if (callback != null)
+            {
+                var text = request.responseText;
+                var response = JSON.parse(text);
+                response["_request_url"] = url;
+                response["_status"] = request.status;
+                callback(response);
+            }
+        }
+    };
+    var asynchronous = true;
+    request.open("GET", url, asynchronous);
+    request.send();
+}
+
 function post(url, data, callback)
 {
     var request = new XMLHttpRequest();
