975ec05ac26029746cc37b6272324df4b35f43a6
Ethan Dalool <edalool@yahoo.com>
2018-03-11 16:19:08 -0700

    Scramble review timestamps.

--- utilities/samplerecipes/samplerecipes.py
+++ utilities/samplerecipes/samplerecipes.py
@@ -2165,9 +2165,15 @@ import random
 AGE_MAX = 30 * 24 * 60 * 60
 now = recipedb.helpers.now()
 cur = rdb.sql.cursor()
-cur.execute('SELECT RecipeID FROM Recipe')
-recipe_ids = [x[0] for x in cur.fetchall()]
-for recipe_id in recipe_ids:
+recipes = rdb.get_recipes()
+for recipe in recipes:
     created = now - random.randint(1, AGE_MAX)
-    cur.execute('UPDATE Recipe SET DateAdded=? WHERE RecipeID=?', [created, recipe_id])
+    cur.execute('UPDATE Recipe SET DateAdded=? WHERE RecipeID=?', [created, recipe.id])
+
+reviews = rdb.get_reviews()
+for review in reviews:
+    recipe = rdb.get_recipe(review.recipe_id)
+    created = random.randint(int(recipe.date_added), int(now))
+    cur.execute('UPDATE Review SET DateAdded=? WHERE ReviewID=?', [created, review.id])
+
 rdb.sql.commit()
